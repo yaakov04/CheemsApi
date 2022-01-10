c8efd838cd8ddca7da387a0aa2a06f452097cd0e
Yaakov <dbrionesmtz@yahoo.com>
2022-01-09 21:17:52 -0600

    configurando la ruta del api y la respuesta

---
+++ app/Http/Controllers/Api/V1/ArticleController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\V1\ArticleResource;
+use App\Models\Article;
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return new ArticleResource(Article::all());
+    }
+
+}
