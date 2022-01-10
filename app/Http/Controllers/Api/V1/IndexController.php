6705cd81401823d7e59cbe710d0a500c93092a68
Yaakov <dbrionesmtz@yahoo.com>
2022-01-10 10:04:42 -0600

    agregando una nueva ruta y nuevo controlador a la api

---
+++ app/Http/Controllers/Api/V1/IndexController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json([
+            'data'=>[
+                route('v1.index'),
+                route('articles.index')
+            ]
+        ]);
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
 use App\Http\Controllers\Api\V1\ArticleController;
+use App\Http\Controllers\Api\V1\IndexController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('v1/articles', ArticleController::class)
+    ->only(['index', 'show']);
+Route::apiResource('v1', IndexController::class)
     ->only('index');
